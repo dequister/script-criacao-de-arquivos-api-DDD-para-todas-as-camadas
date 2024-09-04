43cfa5ab84cccef882378ec91ec87b47400dd309
dequister <p.melchiades777@gmail.com>
2024-09-04 12:24:34 -0300

    perfeito e sem array para as interfaces

--- criacaoDeArquivosApi.ps1
+++ criacaoDeArquivosApi.ps1
@@ -6,14 +6,14 @@ param(
 $caminhoPai = "C:\Work\Meus projetos\ApiPedidos\"
 # Diretórios base para cada camada
 $controllerDir = "$caminhoPai`pedidos-api\Controllers"
-$appServiceDir = "Application\AppService"
-$serviceDir = "Services"
-$repositoryDir = "Repositories"
+$appServiceDir = "$caminhoPai$entityNameSpace.Application\AppService"
+$serviceDir = "$caminhoPai$entityNameSpace.Domain\Service"
+$repositoryDir = "$caminhoPai$entityNameSpace.Data.SqlServer\Repository"
 
 # Criar arquivos de interface
-$interfaceAppService = "$caminhoPai$entityNameSpace`.Application\Interface\I$entityName`AppService.cs"
-$interfaceService = "$caminhoPai$entityNameSpace`.Domain\Interface\Service\I$entityName`Service.cs"
-$interfaceRepository = "$caminhoPai$entityNameSpace`.Domain\Interface\Repository\I$entityName`Repository.cs"
+$interfaceAppService = "$caminhoPai$entityNameSpace.Application\Interface\I$entityName`AppService.cs"
+$interfaceService = "$caminhoPai$entityNameSpace.Domain\Interface\Service\I$entityName`Service.cs"
+$interfaceRepository = "$caminhoPai$entityNameSpace.Domain\Interface\Repository\I$entityName`Repository.cs"
  
 # Criar arquivos de implementação
 $controller = "$controllerDir\$entityName`Controller.cs"
@@ -24,17 +24,26 @@ $repository = "$repositoryDir\$entityName`Repository.cs"
 # Conteúdo base para cada arquivo
 $controllerContent = @"
 using Microsoft.AspNetCore.Mvc;
-using $entityNameSpace`.Application.AppService;
-using $entityNameSpace`.Application.Interface;
-using $entityNameSpace`.Models;
-namespace $entityNameSpace`.Controllers
+using $entityNameSpace.Application.AppService;
+using $entityNameSpace.Application.Interface;
+using $entityNameSpace.Models;
+
+namespace $entityNameSpace.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class $entityName`Controller : ControllerBase
     {
         private readonly I$entityName`AppService _$entityName`AppService;
-        private readonly ILogger<ILogAppService> _logger;
+        private readonly ILogger<$entityName`Controller> _logger;
+
+        public $entityName`Controller(I$entityName`AppService $entityName`AppService, ILogger<$entityName`Controller> logger)
+        {
+            _$entityName`AppService = $entityName`AppService;
+            _logger = logger;
+        }
+
+        // Métodos do controller
     }
 }
 "@
@@ -42,14 +51,12 @@ namespace $entityNameSpace`.Controllers
 $appServiceContent = @"
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using $entityNameSpace`.Application.Interface;
-using $entityNameSpace`.Domain.Interface.Service;
-using $entityNameSpace`.Models;
+using $entityNameSpace.Application.Interface;
+using $entityNameSpace.Domain.Interface.Service;
+using $entityNameSpace.Models;
 
-namespace $entityNameSpace`.Application.AppService
+namespace $entityNameSpace.Application.AppService
 {
     public class $entityName`AppService : I$entityName`AppService
     {
@@ -57,23 +64,23 @@ namespace $entityNameSpace`.Application.AppService
 
         public $entityName`AppService(I$entityName`Service $entityName`Service)
         {
-            this._$entityName`Service = $entityName`Service;
+            _$entityName`Service = $entityName`Service;
         }
+
+        // Métodos do AppService
     }
 }
 "@
 
 $serviceContent = @"
-using $entityNameSpace`.Domain.Interface.Repository;
-using $entityNameSpace`.Domain.Interface.Service;
-using $entityNameSpace`.Models;
+using $entityNameSpace.Domain.Interface.Repository;
+using $entityNameSpace.Domain.Interface.Service;
+using $entityNameSpace.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace $entityNameSpace`.Domain.Service
+namespace $entityNameSpace.Domain.Service
 {
     public class $entityName`Service : I$entityName`Service
     {
@@ -81,83 +88,86 @@ namespace $entityNameSpace`.Domain.Service
 
         public $entityName`Service(I$entityName`Repository $entityName`Repository)
         {
-            this._$entityName`Repository = $entityName`Repository;
+            _$entityName`Repository = $entityName`Repository;
         }
+
+        // Métodos do Service
     }
 }
 "@
 
 $repositoryContent = @"
-using $entityNameSpace`.Data.SqlServer.Repository;
-using $entityNameSpace`.Domain.Interface.Repository;
+using $entityNameSpace.Data.SqlServer.Context;
+using $entityNameSpace.Domain.Interface.Repository;
+using $entityNameSpace.Models;
 using Dapper;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Data;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using $entityNameSpace`.Data.SqlServer.Context;
-using System.Globalization;
-using $entityNameSpace`.Models;
 
-namespace $entityNameSpace`.Data.SqlServer.Repository
+namespace $entityNameSpace.Data.SqlServer.Repository
 {
     public class $entityName`Repository : I$entityName`Repository
     {
         private readonly DbBaseOppContext _dbBaseOppContext;
+
+        public $entityName`Repository(DbBaseOppContext dbBaseOppContext)
+        {
+            _dbBaseOppContext = dbBaseOppContext;
+        }
+
+        // Métodos do Repositório
     }
 }
 "@
 
 $appServiceInterfaceContent = @"
-using $entityNameSpace`.Models;
+using $entityNameSpace.Models;
 
-namespace $entityNameSpace`.Application.Interface
+namespace $entityNameSpace.Application.Interface
 {
     public interface I$entityName`AppService
     {
-
+        // Assinaturas dos métodos do AppService
     }
 }
 "@
 
 $serviceInterfaceContent = @"
-using $entityNameSpace`.Models;
+using $entityNameSpace.Models;
 
-namespace $entityNameSpace`.Domain.Interface.Service
+namespace $entityNameSpace.Domain.Interface.Service
 {
     public interface I$entityName`Service
     {
-
+        // Assinaturas dos métodos do Service
     }
 }
 "@
 
 $repositoryInterfaceContent = @"
-using $entityNameSpace`.Models;
+using $entityNameSpace.Models;
 
-namespace $entityNameSpace`.Domain.Interface.Repository
+namespace $entityNameSpace.Domain.Interface.Repository
 {
     public interface I$entityName`Repository
     {
-
+        // Assinaturas dos métodos do Repositório
     }
 }
 "@
 
 # Criação dos arquivos com o conteúdo
-@($interfaceController, $interfaceAppService, $interfaceService, $interfaceRepository, $controller, $appService, $service, $repository) | ForEach-Object {
+@($interfaceAppService, $interfaceService, $interfaceRepository, $controller, $appService, $service, $repository) | ForEach-Object {
     if (-not (Test-Path $_)) {
         New-Item -ItemType File -Path $_
     }
 }
 
-Set-Content -Path $interfaceController -Value $controllerContent
-Set-Content -Path $interfaceAppService -Value $appServiceContent
-Set-Content -Path $interfaceService -Value $serviceContent
-Set-Content -Path $interfaceRepository -Value $repositoryContent
+Set-Content -Path $interfaceAppService -Value $appServiceInterfaceContent
+Set-Content -Path $interfaceService -Value $serviceInterfaceContent
+Set-Content -Path $interfaceRepository -Value $repositoryInterfaceContent
 
 Set-Content -Path $controller -Value $controllerContent
 Set-Content -Path $appService -Value $appServiceContent
